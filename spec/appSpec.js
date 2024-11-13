98acc75b15c373f4326729d54ced9e115a96e2f0
Josué Samba <josue.sambadianzungu@student.vinci.be>
2024-11-13 18:00:45 +0100

    add : minute 1 of the simple minutes and test for minute 1

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -3,10 +3,17 @@ import { Main } from '../src/app.js';
 describe("Berlin Clock - Simple Minutes", function() {
     const main = new Main();
 
-    it("hould activate a light if the minutes are equal to 0", function () {
+    it("should activate a light if the minutes are equal to 0", function () {
 
         const result = main.simpleMinutes(0);
         
         expect(result).toEqual("OOOO");
+      });
+
+    it("should activate a light if the minutes are equal to 1", function () {
+
+        const result = main.simpleMinutes(1);
+        
+        expect(result).toEqual("YOOO");
       });
     });
--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ export class Main{
 
     simpleMinutes(minutes){
 
+        if (minutes === 0) return "OOOO";
+
+        if (minutes === 1) return "YOOO";
+
         return "OOOO";
     }
 }
